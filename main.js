f8cf87fb232f8b5202864a1fa8c26bab1150df28
Angelika Sowka <sowkaangelika@gmail.com>
2025-03-16 21:30:39 -0400

    add habit

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const habitInput = document.getElementById("habit-input");
 const addHabitBtn = document.getElementById("add-habit-btn");
 const displayList = document.getElementById("habit-output");
+let habitCount = 0;
 
 addHabitBtn.addEventListener("click", function () {
   const habitText = habitInput.value.trim();
   if (habitText === "") return;
 
+  habitCount++;
+
+  const uniqueId = "habit-" + habitCount;
+
   const listItem = document.createElement("li");
-  listItem.textContent = habitText;
+  listItem.classList.add("habit-output-list");
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.value = "value";
+  checkbox.id = uniqueId;
+
+  const label = document.createElement("label");
+  label.htmlFor = uniqueId;
+  label.textContent = habitText;
 
   displayList.appendChild(listItem);
+  listItem.appendChild(checkbox);
+  listItem.appendChild(label);
+
   habitInput.value = "";
 });
